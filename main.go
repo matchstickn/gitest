11501c0599255af96d3727ff792a6479680d0489
stkn <matchstickn@gmail.com>
2025-01-02 21:29:52 -0500

    testing

--- main.go
+++ main.go
@@ -3,5 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Is this working? please")
+	Learning("not")
+	Learning("yes")
+}
+
+func Learning(s string) error {
+	if s == "yes" {
+		fmt.Println("learning tine")
+		return nil
+	}
+	fmt.Println("not learning time")
+	return fmt.Errorf("oops")
 }
